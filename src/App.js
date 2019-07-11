aac1d43128fbefcdb040844cf2deda57023d680a
Szar <aly.hennessy@gmail.com>
2019-07-11 12:56:15 -0400

    Added dark + metal style

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends React.Component {
 			c = 4;
 		
 		this.state = {
-			insult: ''
+			insult: '',
+			active: false,
+			transitioning: false
 		}
 		this.sentences = ["GET A LOAD OF THIS", "i fail to understand how you've become such a", "get out of my way, you sorry excuse for a", "i cannot believe that you are such a", "you're a", "i'm getting really tired of this shit, you", "i can't believe you are even talking to me, you", "fuck you and everyone who looks like you, you"]
 		this.adjectives = ["actual", "serious", "ultimate", "good", "adorable", "cute", "anxious", "average", "evil", "big", "brainless", "busted", "cheap", "cursed", "smelly", "creepy", "dead", "delicate", "delicious", "dirty", "dusty", "old", "fine", "fresh", "fragile", "furry", "gaping", "gigantic", "hideous", "hot", "hungry", "thirsty", "juicy", "lame", "little", "magical", "meaty", "moldy", "nasty", "nervous", "premium", "repulsive", "ripe", "rotten", "rude", "salty", "savory", "scrawny", "sloppy", "spooky", "stupid", "super", "supreme", "tasty", "teeny-tiny", "thick", "ugly", "wacky", "wild", "left-handed", "goofy", "wrinkly", "wide-toed", "gay", "autistic", "snivelling"]
@@ -28,8 +30,21 @@ class App extends React.Component {
 		var s = this.rand(this.sentences),
 			a = this.rand(this.adjectives),
 			n = this.rand(this.nouns),
-			c = this.rand(this.curseWords);
-		this.setState({insult: a+" "+c+" "+n})
+			c = this.rand(this.curseWords),
+			i = a+" "+c+" "+n,
+			t = this;
+		this.setState({
+			active: true,
+			transitioning: this.state.active?true:false,
+			insult: this.state.active?this.state.insult:i
+		})
+
+		setTimeout(function(){
+			t.setState({ 
+				insult: i,
+				transitioning: false 
+			})
+		}, 750)
 	}
 
 
@@ -45,8 +60,8 @@ class App extends React.Component {
 				<div id="insulter">
 					<div className="container">
 						<h1 className="metal"><span>iNsuLteR</span></h1>
-						<div id="insult">
-							<div>{this.state.insult}</div>
+						<div id="insult" className={this.state.active?'active':null}>
+							<div className={this.state.transitioning?'transitioning':null}>{this.state.insult}</div>
 						</div>
 						<div className="btn" onClick={this.insult}>
 							Insult Me
